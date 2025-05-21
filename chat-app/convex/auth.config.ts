bb31b9a9de39eec8f3ee3ada05be673cc7511a37
oshanLahiru0307 <klahiruoshan@gmail.com>
2025-05-21 16:23:48 +0530

    intial commit message

---
+++ chat-app/convex/auth.config.ts
@@ -0,0 +1,9 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    providers: [
+      {
+        domain: "https://trusty-hornet-24.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
